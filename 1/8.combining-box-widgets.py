94cec69077e73f88d273e94cb0ed94910c94c9ce
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-06-02 19:18:24 +0530

    combining box widgets

--- 1/8.combining-box-widgets.py
+++ 1/8.combining-box-widgets.py
@@ -1,4 +1,5 @@
-# textbox button widget disable
+# combining box widgets
+
 import tkinter as tk
 from tkinter import ttk #ttk = themed tk
 
@@ -14,10 +15,19 @@ def clickMe(): #function to change label on click
 
 action = ttk.Button(win, text="Click Me!", command=clickMe) # on click of button that says click me, call function clickMe
 action.grid(column=1, row=0) #location of button
+
+ttk.Label(win,text='Choose a number:').grid(column=1,row=0)
+number = tk.StringVar()
+numberChosen= ttk.Combobox(win, width=12, textvariable=number)
+numberChosen['values'] = (1, 2, 4, 42, 100)
+numberChosen.grid(column=1,row=1)
+numberChosen.current(0)
+
 ttk.Label(win, text="Enter a name:").grid(column=0,row=0) #location of the text label
+
 name = tk.StringVar() # set type of input of name as string
 nameEntered = ttk.Entry(win, width=12, textvariable=name) # take input from application
 nameEntered.grid(column=0,row=1) #location of input box
 nameEntered.focus()
-action.configure(state='disabled')
+
 win.mainloop()
