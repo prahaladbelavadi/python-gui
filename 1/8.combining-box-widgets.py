56328c52563e692fd475a60db76fb7ccfd25e11e
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-06-02 19:22:16 +0530

    change default value and reposition button

--- 1/8.combining-box-widgets.py
+++ 1/8.combining-box-widgets.py
@@ -14,14 +14,14 @@ def clickMe(): #function to change label on click
     action.grid(column=1, row=1)
 
 action = ttk.Button(win, text="Click Me!", command=clickMe) # on click of button that says click me, call function clickMe
-action.grid(column=1, row=0) #location of button
+action.grid(column=2, row=0) #location of button
 
 ttk.Label(win,text='Choose a number:').grid(column=1,row=0)
 number = tk.StringVar()
 numberChosen= ttk.Combobox(win, width=12, textvariable=number)
 numberChosen['values'] = (1, 2, 4, 42, 100)
 numberChosen.grid(column=1,row=1)
-numberChosen.current(0)
+numberChosen.current(3)
 
 ttk.Label(win, text="Enter a name:").grid(column=0,row=0) #location of the text label
 
