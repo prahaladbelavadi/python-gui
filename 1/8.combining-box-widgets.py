6b2cd4aa413779c51753e108957f50120541db42
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-06-02 19:39:41 +0530

    change default value and reposition button

--- 1/8.combining-box-widgets.py
+++ 1/8.combining-box-widgets.py
@@ -18,7 +18,7 @@ action.grid(column=2, row=0) #location of button
 
 ttk.Label(win,text='Choose a number:').grid(column=1,row=0)
 number = tk.StringVar()
-numberChosen= ttk.Combobox(win, width=12, textvariable=number)
+numberChosen= ttk.Combobox(win, width=12, textvariable=number state='readonly')
 numberChosen['values'] = (1, 2, 4, 42, 100)
 numberChosen.grid(column=1,row=1)
 numberChosen.current(3)
